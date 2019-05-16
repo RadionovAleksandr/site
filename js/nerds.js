f0614a5bbf0810e44ba53312884c4b3f810b690f
Radionov Alexandr <Radionovalek@yandex.ru>
2019-05-16 15:15:27 +0800

    slider

--- js/nerds.js
+++ js/nerds.js
@@ -1,45 +1,153 @@
 
-var link = document.querySelector(".conacts__btn");
-var popup = document.querySelector(".modal-window");
-var close = document.querySelector(".modal-window__close-btn");
-var login = document.querySelector("[name=login]");
-var form = document.querySelector(".modal-window__login-form");
-var email = document.querySelector("[name=email]");
-var textarea = document.querySelector("[name=textarea]");
-var storage = localStorage.getItem("login");
-var slideBtn1 = document.querySelector(".slider__switch__btn--1");
-var slideBtn2 = document.querySelector(".slider__switch__btn--2");
-var slideBtn3 = document.querySelector(".slider__switch__btn--3");
-var slideText1 = document.querySelector(".slider__text--1");
-var slideText2 = document.querySelector(".slider__text--2");
-var slideText3 = document.querySelector(".slider__text--3");
-var slideImg1 = document.querySelector(".slider-img--1");
-var slideImg2 = document.querySelector(".slider-img--2");
-var slideImg3 = document.querySelector(".slider-img--3");
-var slide1 = document.querySelectorAll('.slide__1');
-var slide2 = document.querySelectorAll('.slide__2');
-var slide3 = document.querySelectorAll('.slide__3');
+(function() {
+  var link = document.querySelector(".conacts__btn");
+  var popup = document.querySelector(".modal-window");
+  var close = document.querySelector(".modal-window__close-btn");
+  var login = document.querySelector("[name=login]");
+  var form = document.querySelector(".modal-window__login-form");
+  var email = document.querySelector("[name=email]");
+  var textarea = document.querySelector("[name=textarea]");
+  var storage = localStorage.getItem("login");
+  var slideBtn1 = document.querySelector(".slider__switch__btn--1");
+  var slideBtn2 = document.querySelector(".slider__switch__btn--2");
+  var slideBtn3 = document.querySelector(".slider__switch__btn--3");
+  var slideText1 = document.querySelector(".slider__text--1");
+  var slideText2 = document.querySelector(".slider__text--2");
+  var slideText3 = document.querySelector(".slider__text--3");
+  var slideImg = document.querySelector(".slider-img");
+  var slideImg1 = document.querySelector(".slider-img--1");
+  var slideImg2 = document.querySelector(".slider-img--2");
+  var slideImg3 = document.querySelector(".slider-img--3");
+  var slide1 = document.querySelectorAll('.slide__1');
+  var slide2 = document.querySelectorAll('.slide__2');
+  var slide3 = document.querySelectorAll('.slide__3');
+  var sliderImgs = document.querySelectorAll('.slider-img')
 
 
-dataSlide1 = {
-  button: slideBtn1,
-  image: slideImg1,
-  text: slideText1
+  dataSlide1 = {
+    button: slideBtn1,
+    image: slideImg1,
+    text: slideText1
+  }
+
+  dataSlide2 = {
+    button: slideBtn2,
+    image: slideImg2,
+    text: slideText2
+  }
+
+  dataSlide3 = {
+    button: slideBtn3,
+    image: slideImg3,
+    text: slideText3
+  }
+  var slider = [dataSlide1, dataSlide2, dataSlide3];
+  var blockSlider = document.querySelector('.advantages__slider');
+
+  var FOTO_DATA = ['img-nerds/slide1.png', 'img-nerds/slide2.png', 'img-nerds/slide3.png'];
+  var DESCRIPTION_DATA = ['Математически выверенный дизайн для вашео сайта или мобильного приложения',
+   'Никакого креатива только математические формулы для расчета идеальных пропорций',
+    'Работать днем как все? мы против этого.<br>Ближе к полуночи работа только начинается'];
+  var TITLE_DATA = ['Долго, дорого, скрупулезно.', 'Любим математику как никто другой', 'Только ночь только хардкор.'];
+  var buttons = document.querySelectorAll('.slider__switch__btn');
+  sliderClass = ['slider-img--1', 'slider-img--2', 'slider-img--3'];
+
+  
+
+  // слайдер, вариант с подменной данных
+  var clickButton = function(number) {
+     blockSlider.querySelector('.slider-img').src = FOTO_DATA[number];
+     blockSlider.querySelector('.slider__text-description').textContent = DESCRIPTION_DATA[number];
+     blockSlider.querySelector('.slider__title').textContent = TITLE_DATA[number];
+     for(i = 0; i <= 2; i++) {
+      if (buttons[i].classList.contains('slider__switch__btn--visited')) {
+        buttons[i].classList.toggle('slider__switch__btn--visited');
+    }}
+    buttons[number].classList.toggle('slider__switch__btn--visited');
 }
 
-dataSlide2 = {
-  button: slideBtn2,
-  image: slideImg2,
-  text: slideText2
-}
+//обработчики событий
+  buttons[0].addEventListener("click", function() {
+   clickButton(0);
+   slideImg.classList.add('slider-img--1');
 
-dataSlide3 = {
-  button: slideBtn3,
-  image: slideImg3,
-  text: slideText3
-}
-var slider = [dataSlide1, dataSlide2, dataSlide3];
+   /*   if (!buttons[number].classList.contains('slider-img--1')) {
+    slideImg.classList.toggle('slider-img--1');
+   } if (buttons[number].classList.contains('slider-img--2')){
+    slideImg.classList.toggle('slider-img--2');
+   } if (buttons[number].classList.contains('slider-img--2')){
+    slideImg.classList.toggle('slider-img--3');
+   } */
+  });
 
+  buttons[1].addEventListener("click", function() {
+   clickButton(1);
+   slideImg.classList.toggle('slider-img--2');
+  });
+
+  buttons[2].addEventListener("click", function() {
+   clickButton(2);
+   slideImg.classList.toggle('slider-img--3');
+  });
+
+
+  //код слайдера с созданием элементов
+/*
+  (function() {
+    var sliderImg = document.querySelector('.slider-img');
+    var blockSlider = document.querySelector('.advantages__slider');
+    var blockSlider = document.querySelector('.advantages__slider');
+    var section = document.querySelector('.advantages')
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i <= 2; i++) {
+      var sliderElement = blockSlider.cloneNode(true);
+
+      sliderElement.querySelector('.slider-img').src = FOTO_DATA[i];
+      sliderElement.querySelector('.slider__text-description').textContent = DESCRIPTION_DATA[i];
+      sliderElement.querySelector('.slider__title').textContent = TITLE_DATA[i];
+      // sliderImgs[i].classList.add('slider-img--' + i); // пытаюсь добавить i-ой картинке i-ый класс
+
+      if (i != 0) {      //странное поведение если пишу if (i = 0) { sliderElement.classList.toggle('visually-hidden')} 
+                            //результат, бескнечная загрузка страницы
+      } else {
+        sliderElement.classList.toggle('visually-hidden')
+      }
+      sliderElement.classList.add('clone');
+      fragment.appendChild(sliderElement);
+    }
+
+    section.appendChild(fragment);
+
+  })();
+
+  var slider = document.querySelector('.advantages__slider');
+  var sliderClone = document.querySelectorAll('.clone');
+
+  // обработчики событий
+  slideBtn1.addEventListener("click", function() {
+    console.log('кнопка1')
+    for(i = 0; i < 2; i++) {
+      if (!sliderClone[i].classList.contains('visually-hidden')) {
+        sliderClone[i].classList.toggle('visually-hidden');
+    }
+    if (sliderClone[0].classList.contains('visually-hidden')) {
+      sliderClone[0].classList.toggle('visually-hidden');
+    }
+  });
+
+  slideBtn2.addEventListener("click", function() {
+    console.log('кнопка2')
+    slider.classList.toggle('visually-hidden');
+    sliderClone[1].classList.toggle('visually-hidden');
+  });
+
+  slideBtn3.addEventListener("click", function() {
+    console.log('кнопка3')
+    slider.classList.toggle('visually-hidden');
+    sliderClone[2].classList.toggle('visually-hidden');
+  });
+*/
+/*
 var sliderActive = function(curenSlider, numberDisableFirst, numberDisabledSecond) {
   curenSlider.classList.remove("visually-hidden");
   curenSlider.classList.add("slider-show");
@@ -116,4 +224,5 @@ window.addEventListener("keydown", function(evt) {
       popup.classList.remove("modal-show");
     }
   }
-});
+}); */
+})()
